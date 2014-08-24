9a887eefce5ed078ba5d2dd18e1d861255e83378
Jason Loo <jmloo@uwaterloo.ca>
2014-08-24 14:49:49 -0400

    fixed sidebar

--- _site/assets/js/site.js
+++ _site/assets/js/site.js
@@ -12,22 +12,28 @@ function hideRmAnimation(){
 var active = "main";
 $(document).ready(function(){
 	hideAll();
+});
 
-	$('#blog-button').click(function(){
-		hideRmAnimation();
-		active = "post-list";
-		$('#post-list').show().addClass("zoomIn");
-	});
+$('#blog-button').click(function(){
+	hideRmAnimation();
+	active = "post-list";
+	$('#post-list').show().addClass("zoomIn");
+});
 
-	$('#about-me-button').click(function(){
-		hideRmAnimation();
-		active = "about";
-		$('#about').show().addClass("zoomIn");
-	});
+$('#about-me-button').click(function(){
+	hideRmAnimation();
+	active = "about";
+	$('#about').show().addClass("zoomIn");
+});
 
-	$('#projects-button').click(function(){
-		hideRmAnimation();
-		active = "projects";
-		$('#projects').show().addClass("zoomIn");
-	});
+$('#projects-button').click(function(){
+	hideRmAnimation();
+	active = "projects";
+	$('#projects').show().addClass("zoomIn");
+});
+
+$('#main-button').click(function(){
+	hideRmAnimation();
+	active = "main";
+	$('#main').show().addClass("zoomIn");
 });
